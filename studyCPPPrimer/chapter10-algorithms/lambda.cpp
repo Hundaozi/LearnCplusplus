0d985003e1fa3bf455a4d254b590c219db7a3435
Yonghua Li <449086240@qq.com>
2023-04-17 17:59:21 +0800

    捕获引用

--- studyCPPPrimer/chapter10-algorithms/lambda.cpp
+++ studyCPPPrimer/chapter10-algorithms/lambda.cpp
@@ -11,9 +11,23 @@ void fun1()
     v1=0;
     auto j=f();  //j会是42，因为f函数储存了V1的复制值
 }
+
+void fun2()
+{
+    size_t v1=42;
+    
+    auto f2=[&v1] { return v1; };
+
+    v1=0;
+
+    auto j=f2();      //j的值会是0   ，f2只是引用了v1，f2并不存储v1
+}
+
 int main(){
 
-    fun1();
+    // fun1();
+
+    fun2();
 
     return 0;
 }
