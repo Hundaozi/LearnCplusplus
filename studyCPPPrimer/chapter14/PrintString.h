7df8ec5652691206df0a7494192da32250217c53
Yonghua Li <449086240@qq.com>
2023-05-11 16:23:40 +0800

    PrintString.h

---
+++ studyCPPPrimer/chapter14/PrintString.h
@@ -0,0 +1,26 @@
+#ifndef PRINTSTRING_H
+#define PRINTSTRING_H
+
+#include<string>
+#include<iostream>
+
+
+class PrintString{
+    public:
+    PrintString(std::ostream &o=std::cout, char c=' '):
+        os(o), sep(c) {}
+
+    void operator() (const std::string &s) const {os<<s<<sep;}
+
+    private:
+        std::ostream &os;
+        char sep;
+}
+
+
+
+
+
+
+
+#endif PRINTSTRING_H
