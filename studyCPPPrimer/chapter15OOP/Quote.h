1864d7a25d2a9fac1974653a94749077b195c31f
Yonghua Li <449086240@qq.com>
2023-05-12 22:18:38 +0800

    15.1

--- studyCPPPrimer/chapter15OOP/Quote.h
+++ studyCPPPrimer/chapter15OOP/Quote.h
@@ -31,4 +31,12 @@ class Bulk_quote : public Quote{
     private:
         std::size_t min_qty=0;
         double discount=0.0;
+    //这个函数生成折扣价格
+    double Bulk_quote::net_price(std::size_t cnt)const
+    {
+        if(cnt >= min_qty)
+            return cnt * (1 - discount) * price;
+        else
+            return cnt * preice;
+    }
 }
