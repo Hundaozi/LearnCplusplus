01839fe1f283c120c34a9b5ff443ca94aadf7a8d
Yonghua Li <449086240@qq.com>
2023-05-15 20:32:24 +0800

    Basket::total_receipt

--- studyCPPPrimer/chapter15OOP/Quote.h
+++ studyCPPPrimer/chapter15OOP/Quote.h
@@ -1,6 +1,7 @@
 #include<string>
 #include<memory>
 #include<set>
+#include<iostream>
 //基类
 class Quote{
     public:
@@ -76,4 +77,18 @@ class Basket{
                             }
 
         std::multiset<std::shared_ptr<Quote>, decltype(compare)*> items{compare};
+
+    double Basket::total_receipt(std::ostream &os)const
+    {
+        double sum=0.0;
+
+        for(auto iter = items.cbegin();
+                 iter != items.cend();
+                 iter = items.upper_bound(*iter))
+                 {
+                    sum += print_total(os, **iter, items.count(*iter));
+                 }
+        os << "Total Sale: "<< sum <<std::endl;
+        return sum;
+    }
 };
