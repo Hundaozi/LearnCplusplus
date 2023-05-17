e952cc9e7768bbccb01944122de98df3269aff4c
Yonghua Li <449086240@qq.com>
2023-05-17 11:54:24 +0800

    eval

--- studyCPPPrimer/chapter15OOP/Query_base.h
+++ studyCPPPrimer/chapter15OOP/Query_base.h
@@ -83,3 +83,49 @@ inline Query operator|(const Query &lhs, const Query &rhs)
 {
     return std::shared_ptr<Query_base>(new OrQuery(lhs, rhs));
 }
+
+QueryResult
+OrQuery::eval(const TextQuery& text) const
+{
+    auto right = rhs.eval(text), left= lhs.eval(text);
+
+    auto ret_lines=std::make_shared<std::set<line_no>>(left.begin(), left.end());
+
+    ret_lines->inesert(right.begin(), right.end());
+
+    return QueryResult(rep(), ret_lines, left.get_file());
+}
+
+QueryResult
+AndQuery::eval(const TextQuery& text)const
+{
+    auto left=lhs.eval(text) , right =rhs.eval(text);
+
+    auto ret_lines = std::make_shared<std::set<line_no>>();
+
+    set_intersection(left.gebin(), left.end(),
+                     right.begin(), right.end(),
+                     inserter(*ret_lines, ret_lines->begin()));
+    
+    return QueryResult(rep(), ret_lines, left.get_file());
+}
+
+QueryResult
+NotQuery::eval(const TextQuery& text)const 
+{
+    auto result = query.eval(text);
+
+    auto ret_lines=std::make_shared<std::set<line_no>>();
+
+    auto beg=result.begin(), end= result.end();
+
+    auto sz=result.get_file()->size();
+
+    for(size_t n=0;n!=sz;++n){
+        if(beg==end||*beg!=n)
+            ret_lines->insert(n);
+        else if(beg!= end)
+            ++beg;
+    }
+    return QueryResult(rep(), ret_lines, result.get_file());
+}
