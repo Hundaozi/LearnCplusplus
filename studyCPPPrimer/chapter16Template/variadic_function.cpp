c135dbbf5ca83639c0f92774297459d46c420a6d
Yonghua Li <449086240@qq.com>
2023-05-22 16:09:52 +0800

    pack

--- studyCPPPrimer/chapter16Template/variadic_function.cpp
+++ studyCPPPrimer/chapter16Template/variadic_function.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 
+template <typename T> std::string debug_rep(const T &t);
+template <typename T> std::string debug_rep(T *p);
+
+std::string debug_rep(const std::string &);
+std::string debug_rep(char *p)
+{
+    return debug_rep(std::string(p));
+}
+
+
 template<typename T>
 std::ostream &print(std::ostream &os, const T &t)
 {
@@ -13,6 +23,12 @@ std::ostream &print(std::ostream &os, const T &t, const Args&... rest)
     return print(os,rest...);
 } 
 
+
+template <typename... Args>
+std::ostream &errorMsg(std::ostream &os, const Args&... rest)
+{
+    return print(os, debug_rep(rest)...);
+}
 int main()
 {   
     int i=0;
@@ -20,4 +36,5 @@ int main()
     std::string s ="我可真牛逼";
     std::cout<<"世界你好!";
     print(std::cout, i, s, 42);
+
 }
