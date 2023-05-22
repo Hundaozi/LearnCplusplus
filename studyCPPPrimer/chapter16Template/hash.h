7d858c25349a56ae3718ed4cb924204586d689fd
Yonghua Li <449086240@qq.com>
2023-05-22 17:28:38 +0800

    hash

---
+++ studyCPPPrimer/chapter16Template/hash.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"../chapter7-Classes/Sales_data.h"
+
+namespace std{
+    //自定义hash数据结构
+    template<> //
+    struct hash<Sales_data>
+    {
+        typedef size_t result_type;
+        typedef Sales_data argument_type;
+        size_t operator()(const Sales_data& s) const;
+    };
+
+    size_t
+    hash<Sales_data>::operator()(const Sales_data& s) const
+    {
+        return hash<string>() (s.bookNo) ^
+               hash<unsigned>() (s.units_sold) ^
+               hash<double>() (s.revenue);
+    }
+}
