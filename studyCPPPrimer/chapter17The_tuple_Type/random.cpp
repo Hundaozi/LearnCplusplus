63caa1d714dbedff1af640186b8f2f58130ddc83
Yonghua Li <449086240@qq.com>
2023-05-26 16:40:43 +0800

    bad_randVec()

--- studyCPPPrimer/chapter17The_tuple_Type/random.cpp
+++ studyCPPPrimer/chapter17The_tuple_Type/random.cpp
@@ -1,10 +1,22 @@
 #include<iostream>
 #include<random>
+#include<vector>
 
-int main(){
+std::vector<unsigned> bad_randVec()
+{
     std::default_random_engine e;
+    std::uniform_int_distribution<unsigned> u;
 
-    for(size_t i=0; i <10; ++i)
-        std::cout<<e()<<std::endl;
+    std::vector<unsigned> ret;
+    for(size_t i=0; i<100;++i)
+        ret.push_back(u(e));
+    return ret;
+}
+int main(){
+
+    std::vector<unsigned> v1(bad_randVec());
+    std::vector<unsigned> v2(bad_randVec());
+
+    std::cout<<((v1==v2)? "equal " : " not equal ") <<std::endl;
     return 0;
 }
