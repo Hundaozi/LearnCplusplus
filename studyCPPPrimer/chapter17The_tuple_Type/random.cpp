ab697cf8380835c2b2237023c75724dac7a1e7bd
Yonghua Li <449086240@qq.com>
2023-05-26 17:03:01 +0800

    seed

--- studyCPPPrimer/chapter17The_tuple_Type/random.cpp
+++ studyCPPPrimer/chapter17The_tuple_Type/random.cpp
@@ -22,16 +22,36 @@ std::vector<unsigned> good_randVec()
         ret.push_back(u(e));
     return ret;
 }
+
+void func()
+{
+    std::default_random_engine e1;
+    std::default_random_engine e2(2147483646);
+
+    std::default_random_engine e3;
+
+    e3.seed(32767);
+
+    std::default_random_engine e4(32767);
+
+    for(size_t i=0;i!=100;++i){
+        if(e1()==e2())
+            std::cout<<" unseeded match at iteration: "<< i <<std::endl;
+        if(e3()!= e4())
+            std::cout<<" seeded differs at oteration: "<< i <<std::endl;
+    }
+}
 int main(){
 
-    std::vector<unsigned> v1(bad_randVec());
-    std::vector<unsigned> v2(bad_randVec());
+    // std::vector<unsigned> v1(bad_randVec());
+    // std::vector<unsigned> v2(bad_randVec());
 
-    std::cout<<((v1==v2)? "equal " : " not equal ") <<std::endl;
+    // std::cout<<((v1==v2)? "equal " : " not equal ") <<std::endl;
 
-    std::vector<unsigned> v3(good_randVec());
-    std::vector<unsigned> v4(good_randVec());
+    // std::vector<unsigned> v3(good_randVec());
+    // std::vector<unsigned> v4(good_randVec());
 
-    std::cout<<((v3==v4)? "equal " : " not equal ") <<std::endl;
+    // std::cout<<((v3==v4)? "equal " : " not equal ") <<std::endl;
+    func();
     return 0;
 }
