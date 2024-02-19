07890be7c34f156ace7b8329fd7be0baa4322787
Hundaozi <449086240@qq.com>
2024-02-19 21:45:03 +0800

    typescript中的类

---
+++ learnTS/4类.ts
@@ -0,0 +1,29 @@
+// class Greeter{
+//     greeting:string;
+//     constructor(message:string){
+//         this.greeting=message;
+//     }
+//     greet(){
+//         return "你好, "+this.greeting;
+//     }
+// }
+// let greeter=new Greeter("世界");
+
+//继承
+
+// class Animal {
+//     move(distanceInMeters: number = 0) {
+//         console.log(`Animal moved ${distanceInMeters}m.`);
+//     }
+// }
+
+// class Dog extends Animal {
+//     bark() {
+//         console.log('Woof! Woof!');
+//     }
+// }
+
+// const dog = new Dog();
+// dog.bark();
+// dog.move(10);
+// dog.bark();
