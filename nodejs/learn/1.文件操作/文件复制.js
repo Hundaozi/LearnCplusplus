91942edc5b2902e40cfa162be5b773ff77a56060
Hundaozi <449086240@qq.com>
2024-04-18 22:13:50 +0800

    文件复制，失败

---
+++ nodejs/learn/1.文件操作/文件复制.js
@@ -0,0 +1,10 @@
+//复制的逻辑很简单，就是将文件的数据读取出来，然后写入新的空文件中
+
+const fs=require('fs');
+const process=require('process');
+
+const rs=fs.createReadStream('./data.txt');
+//每次只取64kb的数据为一块
+const ws=fs.createWriteStream('./data1111.txt',rs);
+
+console.log(process.memoryUsage());
