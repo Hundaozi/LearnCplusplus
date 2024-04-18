25edbfc6ec10f422cc8b6a86eafca49fa066976b
Hundaozi <449086240@qq.com>
2024-04-18 18:48:56 +0800

    同步写入

--- nodejs/learn/fs.ts
+++ nodejs/learn/fs.ts
@@ -4,12 +4,21 @@
 //require用来导入模块
 const fs=require('fs');
 
-//第二步，写入文件
-fs.writeFile('./test.txt','这个世界是物质组成的', err=>{
-     if(err){
-        console.log('写入失败');
+// //第二步，写入文件
+// fs.writeFile('./test.txt','这个世界是物质组成的', err=>{
+//      if(err){
+//         console.log('写入失败');
+//         return ;
+//      }
+//      console.log('写入成功');
+// });//为什么没有智能提示？
+// //文件如果不存在，那就帮我们自动创建
+
+//同步写入
+fs.writeFileSync('./data.txt','这个世界是物质组成的,而且我们都会死',err=>{
+    if(err){
+        console.log('同步写入失败')
         return ;
-     }
-     console.log('写入成功');
-});//为什么没有智能提示？
-//文件如果不存在，那就帮我们自动创建
+    }
+    console.log('同步写入成功');
+});
