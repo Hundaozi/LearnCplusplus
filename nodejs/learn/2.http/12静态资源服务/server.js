0d549d9f3e3aedfbf74925151aed353b74ed1a97
Hundaozi <449086240@qq.com>
2024-04-25 21:42:39 +0800

    设置网站资源根目录

--- nodejs/learn/2.http/12静态资源服务/server.js
+++ nodejs/learn/2.http/12静态资源服务/server.js
@@ -5,12 +5,15 @@ const server=http.createServer((request,response)=>{
     //获取请求url的路径
     let {pathname}=new URL(request.url,'http://127.0.0.1');
 
+    //声明一个根目录
+    let root=__dirname + '/page';
     //拼接文件路径
-    let filePath = __dirname + '/page' + pathname;
+    let filePath =  root + pathname;
 
     //读取文件，fs 异步api
     fs.readFile(filePath,(err,data)=>{
         if(err){
+            response.setHeader('content-type','text/html;charset=utf-8');
             response.statusCode = 500;
             response.end('文件读取失败...');
             console.log('读取错误');
