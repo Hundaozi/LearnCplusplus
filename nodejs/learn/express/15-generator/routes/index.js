050861c199e787212fd5404be3916ce9d1da4ca9
Hundaozi <449086240@qq.com>
2024-05-09 15:45:57 +0800

    上传文件，查看请求报文

--- nodejs/learn/express/15-generator/routes/index.js
+++ nodejs/learn/express/15-generator/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+//现实网页的表单
+router.get('/portarait',(req,res)=>{
+  res.render('portarait');
+});
+//处理文件上传
+router.post('/portrait',(req,res)=>{
+  res.send('上传成功');
+});
+
 module.exports = router;
