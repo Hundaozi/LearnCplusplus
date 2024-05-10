54c50869daea6ea36f5074fc2c9192aceb8e9cba
Hundaozi <449086240@qq.com>
2024-05-10 11:09:09 +0800

    处理文件上传，并且返回文件位置路径

--- nodejs/learn/express/15-generator/routes/index.js
+++ nodejs/learn/express/15-generator/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const formidable = require('formidable');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -14,7 +14,27 @@ router.get('/portarait',(req,res)=>{
 });
 //处理文件上传
 router.post('/portrait',(req,res)=>{
-  res.send('上传成功');
+  //创建form对象
+  const form = formidable({ 
+    multiples: true,
+    //设置上传文件的保存目录
+    uploadDir: __dirname+'/../public/images',
+    //保持文件后缀
+    keepExtensions:true
+   });
+  //解析请求报文
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    //console.log(fields);
+    //console.log(files);
+    let url='/images/' + files.portrait.newFilename;//应该保存在数据库
+    
+    res.send(url);
+    //res.json({ fields, files });
+  });
 });
 
 module.exports = router;
