e39b7db456776a28ce9291eff34c372b4124ba48
Hundaozi <449086240@qq.com>
2024-05-19 17:40:53 +0800

    增加记录，且完善提醒

--- nodejs/accounts/routes/index.js
+++ nodejs/accounts/routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+
+//导入lowdb
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync');
+const shortid = require('shortid');
+ 
+const adapter = new FileSync(__dirname+ '/../data/db.json')
+const db = low(adapter)
+
+//导入shortid
+const id=require('shortid');
+
+
 //记账本的列表
-router.get('c', function(req, res, next) {
+router.get('/account', function(req, res, next) {
   res.render('list');
 });
 
@@ -12,10 +25,21 @@ router.get('/account/create',function(req,res,next){
   res.render('create');
 });
 
-//新登记录
+//新增加记录
 router.post('/account',(req,res)=>{
   //获取请求体的数据
   console.log(req.body);
-  res.send('添加记录');
+
+  //生产id
+  let id=shortid.generate();
+  //写入数据
+  db.get('accounts').unshift(//数据往前加，后加入的在前面
+    {
+      id:id,
+      ...req.body
+    }).write();
+
+    //成功提醒
+  res.render('success',{msg: '添加成功哦', url: '/account'});
 });
 module.exports = router;
