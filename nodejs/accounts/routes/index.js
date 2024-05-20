0d08c54e1999f13c3e1560ae230e1902c50cad2d
Hundaozi <449086240@qq.com>
2024-05-20 16:07:11 +0800

    删除记录，原来增删查改真的这么简单。

--- nodejs/accounts/routes/index.js
+++ nodejs/accounts/routes/index.js
@@ -5,13 +5,11 @@ var router = express.Router();
 //导入lowdb
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync');
-const shortid = require('shortid');
- 
 const adapter = new FileSync(__dirname+ '/../data/db.json')
 const db = low(adapter)
 
 //导入shortid
-const id=require('shortid');
+const shortid=require('shortid');
 
 
 //记账本的列表
@@ -32,7 +30,7 @@ router.get('/account/create',function(req,res,next){
 //新增加记录
 router.post('/account',(req,res)=>{
   //获取请求体的数据
-  console.log(req.body);
+  // console.log(req.body);
 
   //生产id
   let id=shortid.generate();
@@ -45,5 +43,16 @@ router.post('/account',(req,res)=>{
 
     //成功提醒
   res.render('success',{msg: '添加成功哦', url: '/account'});
+
 });
+
+//删除记录
+router.get('/account/:id',(req,res)=>{
+  //获取params的id参数
+  let id=req.params.id;
+  //删除记录
+  db.get('accounts').remove({id:id}).write();
+  //res.send('删除成功');
+  res.render('success',{msg:'删除成功了',url:'/account'});
+})
 module.exports = router;
