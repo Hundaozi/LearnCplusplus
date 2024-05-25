776e1757558f2432d75d3c6a7ffad73c087b4bfa
Hundaozi <449086240@qq.com>
2024-05-25 17:54:45 +0800

    once操作

--- nodejs/accounts/mongdb/01_连接数据库.js
+++ nodejs/accounts/mongdb/01_连接数据库.js
@@ -1,16 +1,24 @@
 const mongoose=require('mongoose');
 
+//设置strictQuery 为 true
+mongoose.set('strictQuery',true);
+
 //连接mongodb服务
 mongoose.connect('mongodb://127.0.0.1/test');
 
+
+
+
 //设置回调
 
-mongoose.connection.on('open',()=>{
+//官方建议用once，不建议使用on
+//once ,回调函数只执行一次/ 
+mongoose.connection.once('open',()=>{
     console.log('连接成功');
 });
-mongoose.connection.on('error',()=>{
+mongoose.connection.once('error',()=>{
     console.log('连接失败');
 });
-mongoose.connection.on('close',()=>{
+mongoose.connection.once('close',()=>{
     console.log('连接关闭');
 });
